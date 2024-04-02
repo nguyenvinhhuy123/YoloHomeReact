2cf20c1a29f6b8542dae750cb283e2e3fd853dfe
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-04-02 16:12:41 +0700

    feat: navigation between screens and icons setting for NavBar

--- front_end/src/components/NavBar.js
+++ front_end/src/components/NavBar.js
@@ -1,47 +1,66 @@
 import * as React from "react";
 import Tab from "../constants/TabNav";
+
 import HomePage from "../screen/HomePage.js";
-
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { useTheme } from 'react-native-paper';
-
+import ControlPanel from "../screen/ControlPanel.js";
+import UserSetting from "../screen/UserSetting.js";
+import FindDevice from "../screen/FindDevice.js";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { useTheme } from "react-native-paper";
 
 //*Screen name
-const homeName = "HomePage"
+const homeName = "HomePage";
+const controlPanelName = "ControlPanel";
+const userSettingName = "UserSetting";
+const findDeviceName = "findDevice";
 
 export default function NavBar() {
   //TODO: Screen to screen navigation
   //*Remove secondaryContainer that would be appear when icon is focused
   const theme = useTheme();
-  theme.colors.secondaryContainer = "transparent"; 
+  theme.colors.secondaryContainer = "transparent";
 
   //*Tag.Screen: screen to be display (ie. homepage, control panel,...)
   return (
     <Tab.Navigator
+      //Router settings for navigation:
       initialRouteName={HomePage}
-      activeColor="#FFB267"
-      inactiveColor="#F8F8F8"
-      barStyle={{ backgroundColor: '#282424' }}
-      labeled={false} //*Text for screen name under icon is not visible
-      theme={theme}
+      backBehavior="history"
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color }) => {
           let iconName;
           let rn = route.name;
-
+          let iconSize = 25
           if (rn === homeName) {
-            //TODO: Add more iconName and style later
             iconName = focused ? "home" : "home-outline";
+          } else if (rn === controlPanelName) {
+            iconName = focused ? "grid" : "grid-outline";
+          } else if (rn === userSettingName) {
+            iconName = focused ? "person-circle" : "person-circle-outline";
+          } else if (rn === findDeviceName) {
+            iconName = focused ? "search" : "search-outline";
           }
-          //*Place holder icon
-          else iconName = focused ? "add-circle" : "add-circle-outline"
+          else {
+            //*Place holder icon
+            iconName = focused ? "add-circle" : "add-circle-outline";
+          }
 
-          //Return icon for tab bar 
-          return <Ionicons name={iconName} size={25} color={color} />;
+          //Return icon for tab bar
+          return <Ionicons name={iconName} size={iconSize} color={color} />;
         },
-      })}>
+      })}
+      //Design settings for navigation:
+      activeColor="#FFB267"
+      inactiveColor="#F8F8F8"
+      barStyle={{ backgroundColor: "#282424" }}
+      labeled={false} //*Text for screen name under icon is not visible
+      theme={theme}
+    >
       {<Tab.Screen name={homeName} component={HomePage} />}
-      {<Tab.Screen name={"Placeholder"} component={HomePage} />}
+      {<Tab.Screen name={findDeviceName} component={FindDevice} />}
+      {<Tab.Screen name={controlPanelName} component={ControlPanel} />}
+      {<Tab.Screen name={userSettingName} component={UserSetting} />}
+
     </Tab.Navigator>
   );
 }
