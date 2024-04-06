13060eaa2477481c8d2ce17ad83384f08dec00a7
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-04-06 13:51:38 +0700

    feat: move color picker logic to new component. picker panel show last picked color

---
+++ front_end/src/components/LightingControl/LEDColorPicker.js
@@ -0,0 +1,78 @@
+import ColorPicker, { Panel1, Swatches, Preview , HueSlider } from 'reanimated-color-picker';
+import { StyleSheet, View, Text, Modal, Button} from 'react-native';
+import {useState, useRef} from 'react';
+import ColorPickerRef from 'reanimated-color-picker';
+
+const styles = StyleSheet.create({
+    container : {
+        marginHorizontal: 10,
+        width: "90%",
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+    },
+    modalView: {
+        margin: 10,
+        backgroundColor: 'black',
+        borderRadius: 15,
+        padding: 35,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 2,
+        elevation: 3,
+    },
+    lightLEDChanger:{
+        flex: 1,
+        width: '100%',
+        justifyContent: 'space-around',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+})
+export default function LEDColorPicker({onColorChange}) {
+    const pickerRef = useRef("white");
+    const [showModal, setShowModal] = useState(false);
+
+    const onPickedColor = (color) => {
+        onColorChange(color);
+        console.log(color.hex);
+        pickerRef.current = color.hex;
+    }
+    return (
+        <View style={styles.container}>
+            <Button color={'#FFB267'} title='Change LED Color' onPress={() => setShowModal(true)} />
+
+            <Modal transparent={true} visible={showModal} animationType='slide'>
+                <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                    <ColorPicker 
+                    value={pickerRef.current}
+                    style={styles.lightLEDChanger} 
+                    onComplete={onPickedColor}
+                    boundedThumb={true}
+                    >
+                        <Preview />
+                        <Panel1 />
+                        <HueSlider />
+                        <Swatches />
+                        <Button color={'#FFB267'} 
+                        title='Confirm' onPress={() => setShowModal(false)} />
+                    </ColorPicker>
+                </View>
+                </View>
+            </Modal>
+        </View>
+    )
+}
--- front_end/src/components/LightingControl/LightControlContainer.js
+++ front_end/src/components/LightingControl/LightControlContainer.js
@@ -1,9 +1,10 @@
 import { StyleSheet, View, Text, Modal, Button} from 'react-native';
 import * as React from 'react';
-import {useState} from 'react'
+import {useState} from 'react';
 import {Switch} from 'react-native-paper';
 import { LampCeiling  } from 'lucide-react-native';
-import ColorPicker, { Panel1, Swatches, Preview, OpacitySlider, HueSlider } from 'reanimated-color-picker';
+
+import LEDColorPicker from './LEDColorPicker';
 
 const styles = StyleSheet.create({
     container : {
@@ -18,41 +19,24 @@ const styles = StyleSheet.create({
     lightToggleButton: {
         flexDirection: 'column',
     },
-    modalView: {
-        margin: 20,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        padding: 35,
-        alignItems: 'center',
-    },
-    lightLEDChanger:{
-        width: '90%',
-        justifyContent: 'space-between',
-    },
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22,
-      },
-
+    
 })
 export default function LightControlContainer(
     {onLightChange,
     onLEDColorChange}
 ){
     const [isEnabled, setIsEnabled] = useState(false);
-    const [showModal, setShowModal] = useState(false);
-
+    const [lampColor, setLampColor] = useState("#f8f8f8");
     const toggleSwitch = () => {
         setIsEnabled(previousState => !previousState);
         onLightChange(!isEnabled);
     }
     const onColorChange = (color) => {
         const hex = color.hex;
-        console.log(hex)
+        setLampColor(hex);
         onLEDColorChange(hex);
     }
+
     
     return (
         <View style={styles.container}>
@@ -64,26 +48,8 @@ export default function LightControlContainer(
                 onValueChange={toggleSwitch}
                 value={isEnabled}
             ></Switch>
-            <View style={styles.container}>
-                <Button color={'#FFB267'} title='Change LED Color' onPress={() => setShowModal(true)} />
-
-                <Modal transparent={true} visible={showModal} animationType='slide'>
-                    <View style={styles.modalView}>
-                    <ColorPicker 
-                    style={styles.lightLEDChanger} value='red' onComplete={onColorChange}
-                    boundedThumb={true}
-                    >
-                        <Preview />
-                        <Panel1 />
-                        <HueSlider />
-                        <OpacitySlider />
-                        <Swatches />
-                    </ColorPicker>
-                    <Button color={'#FFB267'} title='Confirm' onPress={() => setShowModal(false)} />
-                    </View>
-                </Modal>
-            </View>
-            <LampCeiling color='#F8F8F8'/>
+            <LEDColorPicker onColorChange={onColorChange}></LEDColorPicker>
+            <LampCeiling color={lampColor}/>
         </View>
     )
 }
