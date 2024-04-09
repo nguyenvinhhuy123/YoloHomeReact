40a709514eceee2af6155d832266e2df843e45fd
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-04-09 19:31:00 +0700

    feat: add bedroom background png

---
+++ front_end/src/constants/BedroomBG.js
@@ -0,0 +1,6 @@
+import bedroom_bg from '../assets/image/bedroom_bg.png';
+import { Image } from 'react-native';
+
+const BedroomBG = require('../assets/image/bedroom_bg.png')
+
+export default BedroomBG
